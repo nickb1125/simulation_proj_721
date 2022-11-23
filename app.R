802a2de0ace2a86213caadccf29f1ab96cd97a7d
nrb33 <nrb33@duke.edu>
2022-11-23 14:04:27 -0500

    small fixes to plan and explanation in tab 3

--- app.R
+++ app.R
@@ -29,7 +29,8 @@ library(scatterplot3d)
             #                   4. Estimators: (Define functions to get risk, RR, OR, and RD from vectors for exposure and outcome)
             #                   5. Simulate: (Simulate 1 trial from prior exposure risk, risk_trt, risk_control, etc)
             #                   6. Simulate All: (Uses single simulations in lapply to do all simulations)
-            #                   7. Graphing functions for histograms, true distributions, expected confusion tables based on TPR, FPR, N
+            #                   7. how_off: get exact expected differences for each metric based on settings for 3d plots
+            #                   8. Graphing functions for histograms, true distributions, expected confusion tables based on TPR, FPR, N
     
 
             # Shiny Layout: 
@@ -37,7 +38,8 @@ library(scatterplot3d)
             #                   1. Sliders for inputs of TPR, FPR, Risk Exposure, Risk Control (0n left)
             #                   2. Table for current simulation settings (On top right)
             #                   3. Expected confusion table (middle right)
-            #                   3. Histograms with density plots for each metric (bottom right)
+            #                   4. Histograms with density plots for each metric (bottom right)
+            #                   5. 3D plots (FPR, FNR, bias of metrics) based on inut exposure percent, risks
 
 ######################################################################################################################################################################################
 
@@ -100,7 +102,7 @@ ui <- shinyUI(fluidPage(
         tabPanel("Tables", br(), gt_output( 'settings' ),
                  gt_output( 'confusion' )),
         tabPanel("Simulated", br(), plotOutput( 'histograms' )),
-        tabPanel("Expected Metric Bias Plots", br(), plotlyOutput("threeD_plots"))
+        tabPanel("Expected Metric Bias Plots", br(), plotlyOutput("threeD_plots"), htmlOutput("explain") )
       ),
       
     )
@@ -144,18 +146,15 @@ server <- function(input, output) {
     output$threeD_plots <- renderPlotly({
       get_misclass_plots(input$pct_exposed, input$risk_control, input$risk_treatment)
     })
+    
+    output$explain <- renderText({"\n <b>It is clear from both the simulations and from the expected difference plots that misclassification rate has a positive relationship with bias for all of our metrics.
+    From these plots we see that this relationship is linear for risk difference and non-linear for both odds ratios and relative risk. By adjusting the sliders for risk and percent exposed,
+    one can see that the degree of non-linearity for these variables bias is largely dependent on the risk of prior exposure as well as on the risk in the exposure and control groups.
+    We see that these non-linearities in bias can partially be attributed to the seperation of FPR and FNR, since these rates affect total mislcassifcation rate differently
+    dependent on the prior exposure likliehood. </b>"})
   
 })
 }
-
-
-
-### Figure out deal with not updating for FNR
-
-
-
-
-
 
 
 
